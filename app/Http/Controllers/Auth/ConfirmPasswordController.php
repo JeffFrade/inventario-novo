c00234513640aa446aaddf2aa376147f866af4a8
Jefferson Frade <jefferrson.frade@gmail.com>
2022-01-15 01:29:08 -0300

    Refactored Providers

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Core\Providers\RouteServiceProvider;
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ConfirmsPasswords;
 
 class ConfirmPasswordController extends Controller
