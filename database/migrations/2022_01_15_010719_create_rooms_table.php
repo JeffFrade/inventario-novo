ab44988781c920910cd3699577714eb51333a8fd
Jefferson Frade <jefferrson.frade@gmail.com>
2022-01-15 02:20:58 -0300

    Finished tables

---
+++ database/migrations/2022_01_15_010719_create_rooms_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('room', 50);
+            $table->bigInteger('id_sector')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('id_sector')
+                ->references('id')
+                ->on('sectors')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
---
+++ database/migrations/2022_01_15_014814_create_equipments_table.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEquipmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipments', function (Blueprint $table) {
+            $table->id();
+            $table->string('serial_number', 100);
+            $table->string('barcode', 50);
+            $table->string('item', 200);
+            $table->string('brand', 50);
+            $table->string('model', 50);
+            $table->string('cod_outsourcing', 50)->nullable();
+            $table->text('obs')->nullable();
+            $table->bigInteger('id_room')->unsigned();
+            $table->bigInteger('id_responsible')->unsigned();
+            $table->bigInteger('id_type')->unsigned();
+            $table->text('image')->nullable();
+            $table->text('attach')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('id_room')
+                ->references('id')
+                ->on('rooms')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->foreign('id_responsible')
+                ->references('id')
+                ->on('responsibles')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->foreign('id_type')
+                ->references('id')
+                ->on('equipments_types')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            //$table->primary(['id', 'serial_number', 'barcode']);
+
+            $table->index(['serial_number', 'barcode']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipments');
+    }
+}
